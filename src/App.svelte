ab774908b7d6e40893b3e21b9e756e3f8bc31fa8
Josh Nussbaum <joshnuss@gmail.com>
2020-01-13 03:54:04 -0500

    Adds alt tag and adds placholder image

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,10 @@
 
 <main>
   <div class="carousel">
-    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
-    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
-    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
-    <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+    <a class="carousel-item" href="#two!"><img src="https://via.placeholder.com/150" alt="1"></a>
+    <a class="carousel-item" href="#three!"><img src="https://via.placeholder.com/150" alt="2"></a>
+    <a class="carousel-item" href="#four!"><img src="https://via.placeholder.com/150" alt="3"></a>
+    <a class="carousel-item" href="#five!"><img src="https://via.placeholder.com/150" alt="4"></a>
   </div>
 
   <table class="striped">
